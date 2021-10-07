abd0c6e4da42dc4890f079fddf9b9abae43b005e
jrmtesoro <john.ryan.tesoro@gmail.com>
2021-10-07 20:37:21 +0800

    refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroListComponent } from './pages/heroes/hero-list/hero-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroListComponent
   ],
   imports: [
     BrowserModule
