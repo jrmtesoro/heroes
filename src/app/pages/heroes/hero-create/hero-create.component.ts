41c927539a16a116605f976ef3c4e570744ce1a7
jrmtesoro <john.ryan.tesoro@gmail.com>
2021-10-08 14:20:11 +0800

    added hero-create component

---
+++ src/app/pages/heroes/hero-create/hero-create.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-create',
+  templateUrl: './hero-create.component.html',
+  styleUrls: ['./hero-create.component.css'],
+})
+export class HeroCreateComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
