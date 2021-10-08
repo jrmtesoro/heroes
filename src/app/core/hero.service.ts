a0a2669cde9b8f1c4bc71e256b5d1faf37b9ca66
jrmtesoro <john.ryan.tesoro@gmail.com>
2021-10-08 11:19:54 +0800

    added audit log, and single row fetch

--- src/app/core/hero.service.ts
+++ src/app/core/hero.service.ts
@@ -6,15 +6,20 @@ import { HEROES } from '../pages/heroes/shared/hero-seeder';
 import { AuditLogService } from './audit-log.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroService {
-
-  constructor(private auditLogService: AuditLogService) { }
+  constructor(private auditLogService: AuditLogService) {}
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
-    this.auditLogService.add('Loaded heroes');
+    this.auditLogService.add('HeroService: fetched heroes');
     return heroes;
+  }
+
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find((h) => h.id === id)!;
+    this.auditLogService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
   }
 }
